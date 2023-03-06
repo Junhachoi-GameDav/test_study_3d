f36eca4fb051d5ad9d6b29de24a772b48ad7bb9a
janhachoi <wnsgk7654@gmail.com>
2023-03-06 19:23:47 +0900

    3-6

---
+++ Assets/New test_3d/item_script/item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace sg
+{
+    public class item : MonoBehaviour
+    {
+        [Header("Item Information")]
+        public Sprite item_icon;
+        public string item_name;
+    }
+}
+
