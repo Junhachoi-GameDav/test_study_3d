f0a644e753c11932bf65e703bbe6eb6b2a09d0e6
janhachoi <wnsgk7654@gmail.com>
2023-03-07 20:11:30 +0900

    3-7

---
+++ Assets/New test_3d/health_bar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace sg
+{
+    public class health_bar : MonoBehaviour
+    {
+        public Slider slider;
+
+        public void set_max_health(int max_health)
+        {
+            slider.maxValue = max_health;
+            slider.value = max_health;
+        }
+
+        public void set_cur_health(int cur_health)
+        {
+            slider.value = cur_health;
+        }
+    }
+}
+
---
+++ Assets/New test_3d/player_stats.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace sg
+{
+    public class player_stats : MonoBehaviour
+    {
+        public int health_level = 10;
+        public int max_health;
+        public int cur_health;
+
+        public health_bar h_bar;
+
+        private void Start()
+        {
+            max_health = set_max_health_from_health_level();
+            cur_health = max_health;
+            h_bar.set_max_health(max_health);
+        }
+
+        private int set_max_health_from_health_level()
+        {
+            max_health = health_level * 10;
+            return max_health;
+        }
+
+        public void take_damage(int damage)
+        {
+            cur_health = cur_health - damage;
+
+            h_bar.set_cur_health(cur_health);
+        }
+    }
+}
